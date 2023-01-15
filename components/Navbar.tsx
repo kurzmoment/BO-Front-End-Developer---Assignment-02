80b197d7f74824d8a3b484269e5027b72ed943e2
kurzmoment <matej.kratochvil33@seznam.cz>
2023-01-15 19:15:33 +0100

    cart box added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 function Navbar() {
+  const [visible, setVisible] = useState(false);
   return (
     <nav className="">
       <div className="flex pl-10 pr-10 pt-10 justify-between">
@@ -13,13 +14,35 @@ function Navbar() {
             height={50}
           />
         </div>
-        <div>
+        <div
+          className="cursor-pointer"
+          onClick={() => (visible ? setVisible(false) : setVisible(true))}
+        >
           <Image
             src={"./shopping-cart.svg"}
             alt="shoping cart"
             width={30}
             height={30}
           />
+        </div>
+        <div
+          className={
+            visible
+              ? " w-fit h-fit right-10 mt-[55px] p-2 bg-white absolute border border-slate-600"
+              : "hidden"
+          }
+        >
+          <div className="flex">
+            <p className="text-lg font-bold">Your cart is empty</p>
+            <Image
+              onClick={() => setVisible(false)}
+              src={"./x.svg"}
+              alt={"x"}
+              width={20}
+              height={20}
+              className={"cursor-pointer"}
+            />
+          </div>
         </div>
       </div>
       <div className="border-b-2 border border-slate-400 mt-10 b"></div>
