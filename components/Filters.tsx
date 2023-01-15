f7364bc2b901cc200eb5fac14d4984fd18ddff5f
kurzmoment <matej.kratochvil33@seznam.cz>
2023-01-15 18:50:55 +0100

    filter improved for mobile

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import { products } from "../database";
 
-function Filters() {
+type Props = {
+  visible?: boolean;
+};
+
+function Filters({ visible }: Props) {
   var uniqueFilterEntries = Array.from(
     new Set(products.map((item) => item.category))
   );
-  const [visible, setVisible] = useState(false);
 
   return (
-    <div className="sm:pl-20 sm:pt-10 hidden sm:block">
+    <div
+      className={
+        visible
+          ? "sm:pl-20 sm:pt-10 sm:block block"
+          : "sm:pl-20 sm:pt-10 sm:block hidden"
+      }
+    >
       <h4 className="text-lg font-bold">Category</h4>
       {uniqueFilterEntries.map((p, i) => (
         <div key={i} className="flex gap-x-3 content-center pt-2  ">
           <input
             type="checkbox"
             name={p}
-            id="people"
+            id={p}
             className="accent-slate-900 rounded "
           />
           <label htmlFor={p} className="flex text-lg">
--- components/Products.tsx
+++ components/Products.tsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Filters from "./Filters";
 import ProductsShowcase from "./ProductsShowcase";
 
 function Products() {
+  const [visible, setVisible] = useState(false);
   return (
     <div>
-      <div className="flex justify-between sm:pt-10 pt-5 pl-10 pr-10 sm:pl-20 sm:pr-20">
-        <div className="flex">
+      <div className="flex sm:flex-row flex-col justify-between sm:pt-10 pt-5 pl-10 pr-10 sm:pl-20 sm:pr-20">
+        <div className="flex ">
           <h3 className="font-bold sm:text-2xl">Photography /</h3>
           <h3 className="font-bold text-slate-500 sm:text-2xl">
             &nbsp;Premium Photos
           </h3>
         </div>
-        <div className="hidden sm:flex gap-x-3 self-center">
+        <div className="hidden sm:flex  gap-x-3 self-center">
           <p className="text-slate-500 font-md">Sort By</p>
           <select name="sort" id="sort">
             <option value="price">Price</option>
             <option value="alphabetically ">Alphabetically</option>
           </select>
         </div>
-        <button className="sm:hidden">
-          <Image src={"./Slider.svg"} alt={"slider"} width={30} height={30} />
-        </button>
+        <div className="flex flex-row justify-between sm:hidden absolute right-0 pr-10">
+          <a
+            onClick={() => (visible ? setVisible(false) : setVisible(true))}
+            className=""
+          >
+            <Image src={"./Slider.svg"} alt={"slider"} width={30} height={30} />
+          </a>
+        </div>
+        <div className="sm:hidden">
+          <Filters visible={visible} />
+        </div>
       </div>
       <div className="flex flex-col sm:flex-row ">
         <Filters />
